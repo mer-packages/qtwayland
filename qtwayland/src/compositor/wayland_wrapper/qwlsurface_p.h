76a83f212a4e9a326f2420ecf2c3ba093ff69a24
Giulio Camuffo <giuliocamuffo@gmail.com>
2014-03-11 15:47:54 +0200

    Check if the surface is valid before setting it as focus
    A surface may get its resource destroyed and during the time it takes to
    be destroyed it must not be set as mouse of keyboard focus.

--- qtwayland/src/compositor/wayland_wrapper/qwlsurface_p.h
+++ qtwayland/src/compositor/wayland_wrapper/qwlsurface_p.h
@@ -150,6 +150,7 @@ public:
 
     void enterDeleteGuard();
     void leaveDeleteGuard();
+    inline bool isDestroyed() const { return m_destroyed; }
 
     void setCompositorVisible(bool visible);
 
@@ -192,6 +193,7 @@ private:
 
     bool m_surfaceWasDestroyed;
     bool m_deleteGuard;
+    bool m_destroyed;
 
     void damage(const QRect &rect);
     void setBackBuffer(SurfaceBuffer *buffer);
